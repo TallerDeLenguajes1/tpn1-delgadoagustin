e709a01a502e3678734e068e8281393a895db8ec
Agustin <delgadoagustin147@gmail.com>
2020-03-30 12:38:28 -0300

    Desarrollo del los puntos 2.a 2.b 2.c 2.d 2.e

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
+    int a = 5;
+    int * ptra = &a;
+
+    printf("a. Contenido de ptra: %d\n",*ptra);
+    printf("b. Direccion de memoria almacenada en el puntero ptra: %p\n",ptra);
+    printf("c. Direccion de memoria de la variable a: %p\n",&a);
+    printf("d. Direccion de memoria del puntero ptra: %p\n",&ptra);
+    printf("e. Tamaño de memoria de la variable a: %d bytes\n",sizeof(a));
     
+    getchar();//Detiene el programa
     return 0;
 }
